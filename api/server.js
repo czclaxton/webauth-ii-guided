7b36174222c1c600b5d1c6448c11ee56843ede61
Connor Claxton <czclaxton@gmail.com>
2019-09-17 15:46:08 -0500

    mvp

--- api/server.js
+++ api/server.js
@@ -1,21 +1,37 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
 const server = express();
+
+const sessionConfig = {
+  name: "user session",
+  // THIS SHOULD NOT BE HARD CODED IN
+  // THIS IS FOR AN EXAMPLE
+  secret: "monsoon demons are messing with my gutters",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false, // normally true
+    httpOnly: true // cant access via JS
+  },
+  resave: false,
+  saveUninitialized: false
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
 });
 
 module.exports = server;
